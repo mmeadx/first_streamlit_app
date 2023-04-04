ab272b9bb53ffcb35fc5b9141d76914b635ffebf
Matt Mead <mmeadx@gmail.com>
2023-04-03 21:20:01 -0500

    Update text

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,4 +63,4 @@ if streamlit.button('Get Fruit Load List'):
 fruit_add = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    streamlit.txt(insert_row_snowflake(fruit_add))
+    streamlit.text(insert_row_snowflake(fruit_add))
